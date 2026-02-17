99453b47243562c54a66b0f6783074f0f1659d9e
Komorebi Sakuramochi <komorebi_sakuramochi@icloud.com>
2026-02-17 10:46:37 +0900

    Format user input and claude responce

--- src/kurage/__init__.py
+++ src/kurage/__init__.py
@@ -1,3 +1,5 @@
+from textwrap import indent
+
 import anthropic
 from dotenv import load_dotenv
 from prompt_toolkit import prompt
@@ -9,11 +11,13 @@ def main():
     client = anthropic.Anthropic()
     messages = []
     while True:
-        user_input = prompt("User: ", multiline=True)
+        print("\nUser:\n")
+        user_input = prompt("  | ", multiline=True, prompt_continuation="  | ")
         messages.append({"role": "user", "content": user_input})
         response = client.messages.create(
             model="claude-sonnet-4-5-20250929", max_tokens=1024, messages=messages
         )
         assistant_message = response.content[0].text
-        print(f"\nAssistant: {assistant_message}\n")
+        print("\nAssistant:\n")
+        print(indent(assistant_message, "  | ", predicate=(lambda x: True)))
         messages.append({"role": "assistant", "content": assistant_message})
